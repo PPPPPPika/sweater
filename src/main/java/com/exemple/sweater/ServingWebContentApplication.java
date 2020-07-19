e47cf92f273ad083159d5d92529d55e3d76a6757
PPPPPPika <edik_sidorov@list.ru>
2020-07-19 16:42:27 +0300

    19.07.2020/16:41

---
+++ src/main/java/com/exemple/sweater/ServingWebContentApplication.java
@@ -0,0 +1,11 @@
+package com.exemple.sweater;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+}
